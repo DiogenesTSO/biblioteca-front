0763b9c79fe32be2faf580a3930c73555387294f
DiogenesTSO <diogenestobias@hotmail.com>
2024-05-23 20:39:22 -0300

    Rota para login criada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
 import { AlunoListComponent } from './components/aluno/aluno-list/aluno-list.component';
+import { LoginComponent } from './components/login/login.component';
 
 //Criar rotas para redirecionar as paginas
 const routes: Routes = [
+  {path: 'login', component: LoginComponent},
   {
     path: '', component: NavComponent, children: [
       {path: 'home', component: HomeComponent},
